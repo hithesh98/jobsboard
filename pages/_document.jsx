d067edce84e832af4fefaa929c2a81611a4d81c2
hithesh98 <kirrkirry@gmail.com>
2021-07-05 17:18:31 +0100

    add ga

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../lib/gtag'
+
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -12,6 +15,21 @@ class MyDocument extends Document {
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}/>
         </Head>
         <body>
           <Main />
