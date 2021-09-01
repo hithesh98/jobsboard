535915c4d01f0d8f188ee9d326c1b5c6d16b6c54
hithesh98 <kirrkirry@gmail.com>
2021-09-01 13:45:41 +0100

    initial state for search from url query

--- pages/search/index.js
+++ pages/search/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import { InstantSearch, SearchBox, Hits} from 'react-instantsearch-dom'
 import algoliasearch from 'algoliasearch/lite';
-import { useRouter } from 'next/router'
+import Head from 'next/head'
 
 
 
@@ -10,15 +10,18 @@ const Hit = ({ hit }) => <p>{hit.jobTitle}</p>;
 const searchClient = algoliasearch('VLJVAJ9ZX8', '0fddb2ff13078cbddcd2cec602fedc76')
 
 export default function Search() {
-    const router = useRouter()
-    const {input} = router.query
     return (
+        <>
+        <Head>
+            <title>Search</title>
+        </Head>
         <Layout>
             <h1> I am Search</h1> 
-            <InstantSearch indexName='jobs' searchClient={searchClient} searchState={{query:`${input}`}}>
+            <InstantSearch indexName='jobs' searchClient={searchClient} searchState={{query:''}}>
               <SearchBox searchAsYouType={false}/>
               <Hits hitComponent={Hit}/>
             </InstantSearch>
         </Layout>
+        </>
     )
 }
