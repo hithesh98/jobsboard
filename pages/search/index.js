fb43822c733e99541f96f07a21e1d8cb4f182ccf
hithesh98 <kirrkirry@gmail.com>
2021-09-02 18:06:12 +0100

    pagination

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../../components/Layout'
-import { InstantSearch, SearchBox, ClearRefinements, RefinementList,Panel,ToggleRefinement} from 'react-instantsearch-dom'
+import { InstantSearch, SearchBox, ClearRefinements, RefinementList,Panel,ToggleRefinement, Pagination} from 'react-instantsearch-dom'
 import algoliasearch from 'algoliasearch/lite';
 import Head from 'next/head'
 import JobList from '../../components/JobList';
@@ -102,7 +102,54 @@ export default function Search() {
                   </Panel>
                 </div>
                 <div className={styles.hitsWrapper}>
-                  <CustomHits />
+                  <CustomHits />                  
+                  <footer className="container-footer">
+                    <Pagination
+                      padding={2}
+                      showFirst={false}
+                      showLast={false}
+                      translations={{
+                        previous: (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            viewBox="0 0 10 10"
+                          >
+                            <g
+                              fill="none"
+                              fillRule="evenodd"
+                              stroke="#000"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="1.143"
+                            >
+                              <path d="M9 5H1M5 9L1 5l4-4" />
+                            </g>
+                          </svg>
+                        ),
+                        next: (
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            viewBox="0 0 10 10"
+                          >
+                            <g
+                              fill="none"
+                              fillRule="evenodd"
+                              stroke="#000"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="1.143"
+                            >
+                              <path d="M1 5h8M5 9l4-4-4-4" />
+                            </g>
+                          </svg>
+                        ),
+                      }}
+                    />
+                </footer>
                 </div>
               </div>
             </InstantSearch>
