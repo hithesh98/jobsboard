af49322b30e880c004892776ffef887950138787
hithesh98 <kirrkirry@gmail.com>
2021-09-19 15:10:12 +0100

    next and previous buttons working

--- pages/resources/blog/page/[number].jsx
+++ pages/resources/blog/page/[number].jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from '../../../../components/Layout'
 import { getPagePosts } from '../../../../lib/posts'
 import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
 
 
 export async function getStaticPaths(context){
   const posts = await getPagePosts(1);
   const numberPages = posts.meta.pagination.pages
   let arPages = []
-  for(let i=1; i<numberPages; i++){
+  for(let i=1; i<=numberPages; i++){
     arPages.push(i.toString())
   }
   const paths = arPages.map(page=>{
@@ -25,6 +26,8 @@ export async function getStaticPaths(context){
 
 export async function getStaticProps(context) {
     const posts = await getPagePosts(context.params.number)
+    const lastPage = posts.meta.pagination.pages
+
   
     if (!posts) {
       return {
@@ -33,11 +36,27 @@ export async function getStaticProps(context) {
     }
   
     return {
-      props: { posts }
+      props: { posts, lastPage }
     }
   }
 
-export default function Index({posts}) {
+export default function Index({posts, lastPage}) {
+    const router = useRouter()
+    const { number } = router.query
+    const [pageNum, setPageNum] = useState(parseInt(number))
+
+    const onNext = () => {
+      setPageNum(pageNum + 1)
+    }
+    
+    const onPrevious = () => {
+      setPageNum(pageNum - 1)
+    }
+
+    useEffect(() => {
+      router.push(`/resources/blog/page/${pageNum}`)
+    }, [pageNum])
+
     return (
         <Layout>
              <ul>
@@ -49,7 +68,8 @@ export default function Index({posts}) {
                 </li>
             ))}
             </ul>
-            <button> Next </button>
+            {pageNum !== 1 ? <button onClick={onPrevious}> Previous </button> : null}
+            {pageNum !== lastPage ? <button onClick={onNext}> Next </button> : null}
         </Layout>
     )
 }
