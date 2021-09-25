407858ec8d77004564154d8ea4e0c24eb7f1c4dc
Brian Cronin <croninb1@tcd.ie>
2021-09-25 15:56:14 +0100

    Update terms.jsx
    Added in the text

--- pages/terms.jsx
+++ pages/terms.jsx
@@ -14,55 +14,70 @@ export default function Home() {
         </Head>
         <main>
           <Layout>
-            <div>
-              <section className={styles.heroWrapper}>
-                <article className={styles.copyWrapper}> 
-                  <h1 className={styles.header}>Discover jobs at innovative healthcare and lifesciences companies.</h1>
-                  <h3>Sign-up below to get curated job alerts every week.</h3>
-                  <section className="email-wrapper">
-                  <div id="mc_embed_signup">
-                    <form action="https://jobsinhealthtech.us6.list-manage.com/subscribe/post?u=30087181452091fa46daaef74&amp;id=13a887731e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
-                        <div id={styles.mc_embed_signup_scroll}>
-                          <input type="email" name="EMAIL" className="email" id={styles.mceEMAIL} placeholder="Email Address" required/>
-                          <div className={styles.clear}><input type="submit" value="Subscribe" name="subscribe" id={styles.mcEmbeddedSubscribe} className="button"/></div>
-                        </div>
-                    </form>
-                  </div>
-                </section>
-                </article> 
-                <div className={styles.background}>
-                  <Image src ={background} alt='Doctor surrounded by technology items.'/>
-                </div>
-              </section>
-            </div>
-            <div className={styles.jobPage}>
-              <h2 className={styles.categoryTitle}>Engineering Jobs</h2>
-              <HomeJobPage category={'engineering'}/>
-              
-              <h2 className={styles.categoryTitle}>Commercial Jobs</h2>
-              <HomeJobPage category={'commercial'}/>
+          <h2 id="terms-of-service">Terms of Service</h2>
+<p>Last Updated: 22/09/21</p>
+<h3 id="preamble">Preamble</h3>
+<p>Thanks for using JobsinHealthTech, we hope it has helped you find a job or hire someone in Health-tech or lifesciences. Because we don’t know every one of our users personally, we have to put in place some Terms of Service to help keep the ship afloat.</p>
+<p>When we say “Company”, “we”, “our”, or “us” in this document, we are referring to Jobsinhealthtech.com.</p>
+<p>When we say “Services”, we mean any product created and maintained by Jobsinhealthtech.com. That includes jobsinhealthtech.com, whether delivered within a web browser, desktop application, mobile application, or another format.</p>
+<p>We may update these Terms of Service in the future. When you use our Services, now or in the future, you are agreeing to the latest Terms of Service. That’s true for any of our existing and future products and all features that we add to our Services over time. There may be times where we do not exercise or enforce any right or provision of the Terms of Service; in doing so, we are not waiving that right or provision. These terms do contain a limitation of our liability.</p>
+<p>If you violate of any of the terms, we may terminate your account. That’s a broad statement and it means you need to place a lot of trust in us. We do our best to deserve that trust by working hard to provide a great product and being responsive to your questions and feedback.</p>
+<h2 id="account-terms">Account Terms</h2>
+<ol>
+<li><p>You are responsible for maintaining the security of your account. The Company cannot and will not be liable for any loss or damage from your failure to comply with this security obligation.</p>
+</li>
+<li><p>You are responsible for all content posted and activity that occurs under your account. That includes content posted by others who either: (a) have access to your login credentials; or (b) have their own logins under your account.</p>
+</li>
+<li><p>You must be a human. Accounts registered by “bots” or other automated methods are not permitted.</p>
+</li>
+</ol>
+<h2 id="cancellation-and-termination">Cancellation and Termination</h2>
+<ol>
+<li><p>You are solely responsible for properly canceling your account. If you need help cancelling your account, you can always contact us.</p>
+</li>
+<li><p>All of your content will be inaccessible from the Services immediately upon cancellation. We cannot recover this information once it has been permanently deleted.</p>
+</li>
+<li><p>We have the right to suspend or terminate your account and refuse any and all current or future use of our Services for any reason at any time. Suspension means you and any other users on your account will not be able to access the account or any content in the account. Termination will furthermore result in the deletion of your account or your access to your account, and the forfeiture and relinquishment of all content in your account. We also reserve the right to refuse the use of the Services to anyone for any reason at any time. We have this clause because statistically speaking, out of the hundreds of thousands of accounts on our Services, there is at least one doing something nefarious. There are some things we staunchly stand against and this clause is how we exercise that stance.</p>
+</li>
+<li><p>Verbal, physical, written or other abuse (including threats of abuse or retribution) of Company employee or officer will result in immediate account termination.</p>
+</li>
+</ol>
+<h2 id="uptime-security-and-privacy">Uptime, Security, and Privacy</h2>
+<ol>
+<li><p>Your use of the Services is at your sole risk. We provide these Services on an “as is” and “as available” basis. We do not offer service-level agreements for most of our Services, but do take uptime of our website seriously.</p>
+</li>
+<li><p>We reserve the right to temporarily disable your account if your usage significantly exceeds the average usage of other customers of the Services. Of course, we’ll reach out to the account owner before taking any action except in rare cases where the level of use may negatively impact the performance of the Service for other customers.</p>
+</li>
+<li><p>We take many measures to protect and secure your data through backups, redundancies, and encryption. Please contact us to report a security incident or threat.</p>
+</li>
+<li><p>We use third party vendors and hosting partners to provide the necessary hardware, software, networking, storage, and related technology required to run the Services.</p>
+</li>
+</ol>
+<h2 id="copyright-and-content-ownership">Copyright and Content Ownership</h2>
+<ol>
+<li><p>All content posted on the Services must comply with U.S. copyright law. </p>
+</li>
+<li><p>We claim no intellectual property rights over the material you provide to the Services. All materials uploaded remain yours.</p>
+</li>
+<li><p>We do not pre-screen content, but reserve the right (but not the obligation) in our sole discretion to refuse or remove any content that is available via the Service.</p>
+</li>
+<li><p>The names, look, and feel of the Services are copyright© to the Company. All rights reserved. You may not duplicate, copy, or reuse any portion of the HTML, CSS, JavaScript, or visual design elements without express written permission from the Company. You must request permission to use the Company’s logo or any Service logos for promotional purposes. Please email us requests to use logos. We reserve the right to rescind this permission if you violate these Terms of Service.</p>
+</li>
+<li><p>You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Services, use of the Services, or access to the Services without the express written permission by the Company.</p>
+</li>
+<li><p>You must not modify another website so as to falsely imply that it is associated with the Services or the Company.</p>
+</li>
+</ol>
+<h2 id="features-and-bugs">Features and Bugs</h2>
+<p>We design our Services with care, based on our own experience and the experiences of customers who share their time and feedback. However, there is no such thing as a service that pleases everybody. We make no guarantees that our Services will meet your specific requirements or expectations.</p>
+<p>As with any software, our Services inevitably have some bugs. We track the bugs reported to us and work through priority ones, especially any related to security or privacy. Not all reported bugs will get fixed and we don’t guarantee completely error-free Services.</p>
+<h2 id="liability">Liability</h2>
+<p>We mention liability throughout these Terms but to put it all in one section:</p>
+<p>You expressly understand and agree that the Company shall not be liable, in law or in equity, to you or to any third party for any direct, indirect, incidental, lost profits, special, consequential, punitive or exemplary damages, including, but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if the Company has been advised of the possibility of such damages), resulting from: (i) the use or the inability to use the Services; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into through or from the Services; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third party on the service; (v) or any other matter relating to this Terms of Service or the Services, whether as a breach of contract, tort (including negligence whether active or passive), or any other theory of liability.</p>
+<p>In other words: choosing to use our Services does mean you are making a bet on us. If the bet does not work out, that’s on you, not us. We do our darndest to be as safe a bet as possible through careful management of the business; investments in security, infrastructure, and talent; and in general, giving a damn. If you choose to use our Services, thank you for betting on us.</p>
+<p>If you have a question about any of the Terms of Service, please contact us at brian@jobsinhealthtech.com.</p>
+<p>These terms are adapted from the <a href="https://github.com/basecamp/policies">Basecamp open-source policies</a>. Many thanks to the team at <a href="https://basecamp.com/">Basecamp</a> for providing them under the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a> license.**</p>
 
-              <h2 className={styles.categoryTitle}>Clinical / Research Jobs</h2>
-              <HomeJobPage category={'clinical'}/>
-
-              <h2 className={styles.categoryTitle}>Product Jobs</h2>
-              <HomeJobPage category={'product'}/>
-
-              <h2 className={styles.categoryTitle}>Data Jobs</h2>
-              <HomeJobPage category={'data'}/>
-
-              <h2 className={styles.categoryTitle}>Marketing Jobs</h2>
-              <HomeJobPage category={'marketing'}/>
-
-              <h2 className={styles.categoryTitle}>Legal / HR / Finance Jobs</h2>
-              <HomeJobPage category={'legalhrfin'}/>
-
-              <h2 className={styles.categoryTitle}>Customer Success / Operations Jobs</h2>
-              <HomeJobPage category={'customersupportops'}/>
-
-              <h2 className={styles.categoryTitle}>Other Jobs</h2>
-              <HomeJobPage category={'other'}/>
-            </div>
           </Layout>
         </main>
       </div>
