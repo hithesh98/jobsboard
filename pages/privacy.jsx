6e13a2c799bceaf5905ae02fbdced531fcbc6875
Brian Cronin <croninb1@tcd.ie>
2021-09-25 16:25:44 +0100

    Added div
    Added a div with class that has values for left-align. Should work this time.

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -14,7 +14,8 @@ export default function Home() {
         </Head>
         <main>
           <Layout>
-          <h2 id="privacy-policy">Privacy Policy</h2>
+          <div className={styles.categoryTitle}
+          <h2>Privacy Policy</h2>
 <p>Last updated: 22/09/2021</p>
 <p>Your privacy is important to us. It is the policy of Jobsinhealthtech.com,, which owns and operates JobsinHealthTech.com, to respect your privacy regarding any information we may collect from you on our website, <a href="https://jobsinhealthtech.com">https://jobsinhealthtech.com</a>.</p>
 <p>We only ask for personal information when we need it to provide a service to you. We collect it by fair and lawful means, with your knowledge and consent. We also let you know why we’re collecting it and how it will be used.</p>
@@ -23,9 +24,9 @@ export default function Home() {
 <p>Our website may link to external sites that are not operated by us. Please be aware that we have no control over the content and practices of these sites, and cannot accept responsibility or liability for their respective privacy policies.</p>
 <p>You are free to refuse our request for your personal information, with the understanding that we may be unable to provide you with some of your desired services.</p>
 <p>Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.</p>
-<h3 id="questions">Questions</h3>
+<h3>Questions</h3>
 <p>Have any questions, comments, or concerns about this privacy policy, your data, or your rights with respect to your information? Please get in touch by emailing us at brian@jobsinhealthtech.com and we’ll be happy to answer them.**</p>
-
+        </div>
           </Layout>
         </main>
       </div>
--- pages/terms.jsx
+++ pages/terms.jsx
@@ -14,15 +14,16 @@ export default function Home() {
         </Head>
         <main>
           <Layout>
-          <h2 id="terms-of-service">Terms of Service</h2>
+          <div className={styles.categoryTitle}
+          <h2>Terms of Service</h2>
 <p>Last Updated: 22/09/21</p>
-<h3 id="preamble">Preamble</h3>
+<h3>Preamble</h3>
 <p>Thanks for using JobsinHealthTech, we hope it has helped you find a job or hire someone in Health-tech or lifesciences. Because we don’t know every one of our users personally, we have to put in place some Terms of Service to help keep the ship afloat.</p>
 <p>When we say “Company”, “we”, “our”, or “us” in this document, we are referring to Jobsinhealthtech.com.</p>
 <p>When we say “Services”, we mean any product created and maintained by Jobsinhealthtech.com. That includes jobsinhealthtech.com, whether delivered within a web browser, desktop application, mobile application, or another format.</p>
 <p>We may update these Terms of Service in the future. When you use our Services, now or in the future, you are agreeing to the latest Terms of Service. That’s true for any of our existing and future products and all features that we add to our Services over time. There may be times where we do not exercise or enforce any right or provision of the Terms of Service; in doing so, we are not waiving that right or provision. These terms do contain a limitation of our liability.</p>
 <p>If you violate of any of the terms, we may terminate your account. That’s a broad statement and it means you need to place a lot of trust in us. We do our best to deserve that trust by working hard to provide a great product and being responsive to your questions and feedback.</p>
-<h2 id="account-terms">Account Terms</h2>
+<h2>Account Terms</h2>
 <ol>
 <li><p>You are responsible for maintaining the security of your account. The Company cannot and will not be liable for any loss or damage from your failure to comply with this security obligation.</p>
 </li>
@@ -31,7 +32,7 @@ export default function Home() {
 <li><p>You must be a human. Accounts registered by “bots” or other automated methods are not permitted.</p>
 </li>
 </ol>
-<h2 id="cancellation-and-termination">Cancellation and Termination</h2>
+<h2>Cancellation and Termination</h2>
 <ol>
 <li><p>You are solely responsible for properly canceling your account. If you need help cancelling your account, you can always contact us.</p>
 </li>
@@ -42,7 +43,7 @@ export default function Home() {
 <li><p>Verbal, physical, written or other abuse (including threats of abuse or retribution) of Company employee or officer will result in immediate account termination.</p>
 </li>
 </ol>
-<h2 id="uptime-security-and-privacy">Uptime, Security, and Privacy</h2>
+<h2>Uptime, Security, and Privacy</h2>
 <ol>
 <li><p>Your use of the Services is at your sole risk. We provide these Services on an “as is” and “as available” basis. We do not offer service-level agreements for most of our Services, but do take uptime of our website seriously.</p>
 </li>
@@ -53,7 +54,7 @@ export default function Home() {
 <li><p>We use third party vendors and hosting partners to provide the necessary hardware, software, networking, storage, and related technology required to run the Services.</p>
 </li>
 </ol>
-<h2 id="copyright-and-content-ownership">Copyright and Content Ownership</h2>
+<h2>Copyright and Content Ownership</h2>
 <ol>
 <li><p>All content posted on the Services must comply with U.S. copyright law. </p>
 </li>
@@ -68,16 +69,16 @@ export default function Home() {
 <li><p>You must not modify another website so as to falsely imply that it is associated with the Services or the Company.</p>
 </li>
 </ol>
-<h2 id="features-and-bugs">Features and Bugs</h2>
+<h2>Features and Bugs</h2>
 <p>We design our Services with care, based on our own experience and the experiences of customers who share their time and feedback. However, there is no such thing as a service that pleases everybody. We make no guarantees that our Services will meet your specific requirements or expectations.</p>
 <p>As with any software, our Services inevitably have some bugs. We track the bugs reported to us and work through priority ones, especially any related to security or privacy. Not all reported bugs will get fixed and we don’t guarantee completely error-free Services.</p>
-<h2 id="liability">Liability</h2>
+<h2>Liability</h2>
 <p>We mention liability throughout these Terms but to put it all in one section:</p>
 <p>You expressly understand and agree that the Company shall not be liable, in law or in equity, to you or to any third party for any direct, indirect, incidental, lost profits, special, consequential, punitive or exemplary damages, including, but not limited to, damages for loss of profits, goodwill, use, data or other intangible losses (even if the Company has been advised of the possibility of such damages), resulting from: (i) the use or the inability to use the Services; (ii) the cost of procurement of substitute goods and services resulting from any goods, data, information or services purchased or obtained or messages received or transactions entered into through or from the Services; (iii) unauthorized access to or alteration of your transmissions or data; (iv) statements or conduct of any third party on the service; (v) or any other matter relating to this Terms of Service or the Services, whether as a breach of contract, tort (including negligence whether active or passive), or any other theory of liability.</p>
 <p>In other words: choosing to use our Services does mean you are making a bet on us. If the bet does not work out, that’s on you, not us. We do our darndest to be as safe a bet as possible through careful management of the business; investments in security, infrastructure, and talent; and in general, giving a damn. If you choose to use our Services, thank you for betting on us.</p>
 <p>If you have a question about any of the Terms of Service, please contact us at brian@jobsinhealthtech.com.</p>
-<p>These terms are adapted from the <a href="https://github.com/basecamp/policies">Basecamp open-source policies</a>. Many thanks to the team at <a href="https://basecamp.com/">Basecamp</a> for providing them under the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a> license.**</p>
-
+<p>These terms are adapted from the <a href="https://github.com/basecamp/policies">Basecamp open-source policies</a>. Many thanks to the team at <a href="https://basecamp.com/">Basecamp</a> for providing them under the <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution</a> license.</p>
+      </div>
           </Layout>
         </main>
       </div>
