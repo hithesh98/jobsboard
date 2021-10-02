2728d07380a61017da50d028290825b56fbbd1d8
hithesh98 <kirrkirry@gmail.com>
2021-10-02 10:52:44 +0100

    jobs links for everywhere

--- pages/job-id/[id].js
+++ pages/job-id/[id].js
@@ -9,7 +9,8 @@ import DOMPurify from 'isomorphic-dompurify'
 export default function Job() {
     const router = useRouter()
     const {id} = router.query
-    const { data, error } = useSWR(`/api/job-id/${id}`)
+    const idNum = id.slice(0, id.indexOf('-'))
+    const { data, error } = useSWR(`/api/job-id/${idNum}`)
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
     return (
