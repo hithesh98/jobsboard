faaff62f5875e8c0c1afb44b9ca76f79c3b9b925
hithesh98 <kirrkirry@gmail.com>
2021-10-07 12:03:35 +0100

    add google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,17 @@ import './styles/styles.css'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import * as gtag from '../lib/gtag'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  id: "GTM-TVK7PSL",
+}
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs)
+  }, [])
+  
   const router = useRouter()
   useEffect(() => {
     const handleRouteChange = (url) => {
@@ -14,6 +23,7 @@ const App = ({ Component, pageProps }) => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
   }, [router.events])
+
 
   return <Component {...pageProps} />
 }
