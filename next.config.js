d5baa10f0e02f035d1f903eace6f70bea613e8d6
hithesh98 <kirrkirry@gmail.com>
2021-10-17 15:24:25 +0100

    add convertflow and delete mailchimp popup

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
   reactStrictMode: true,
   env: {
     drift: fs.readFileSync('./lib/drift.js').toString(),
-    mailchimp: fs.readFileSync('./lib/mailchimp.js').toString()
   },
   images: {
     domains: ['images.unsplash.com', 'res-3.cloudinary.com', 'hithesh98.github.io'],
