731b07c388ffe593ad0a6ec8b2e37f9060d9cc50
hithesh98 <kirrkirry@gmail.com>
2021-10-20 11:17:16 +0100

    post copy change and styled2

--- pages/post.jsx
+++ pages/post.jsx
@@ -11,10 +11,32 @@ export default function post() {
                 <title>Post a Job</title>
             </Head>
             <Layout>
-                <h1>Reach the top talent in health tech</h1>
-                <p>Contact us at alerts@jobsinhealthtech.com to speak about subscription packages.</p>
-                <p><strong>€80</strong> to publish job listing for <strong>30 days</strong>.</p>
+                <div className={styles.contentWrapper}>
+                    <div className={styles.topCopy}>
+                        <h1>Thanks for your interest in posting a job!</h1>
+                        <p>The cost is €80 to publish a job listing for 30 days. This includes the following:</p>
+                        <ul>
+                            <li>
+                            Featured post on our jobs board with unique page so that you can share the posting
+                            </li>
+                            <li>
+                            Shared post on socials
+                            </li>
+                            <li>
+                            Customer support from Jobs in Health Tech Team
+                            </li>
+                            <li>
+                            Metrics from the job posting if requested
+                            </li>
+                        </ul>
+                        <p>Fill out the Typeform below and then pay with secure stripe checkout. Your post will then be live within 24 hours.</p>
+                        <p>Any questions? Reach out at Brian@jobsinhealthtech.com</p>
+                    </div>
                 <Widget id="Kbk7VlPw" className= {styles.typeform} />
+                <div className={styles.bottomCopy}>
+                    <p>If you are interested in speaking to us about subscription packages or discounts on multiple job postings, please contact us at Brian@jobsinhealthtech.com</p>
+                </div>
+            </div>
             </Layout>
         </>
     )
